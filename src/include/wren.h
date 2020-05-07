be12d450aa3ae480e7c65feaebb4246b5e417d5f
Chevy Ray Johnston <happytrash@gmail.com>
2020-05-06 22:29:01 -0700

    expose wrenReallocate()

--- src/include/wren.h
+++ src/include/wren.h
@@ -288,6 +288,24 @@ WrenVM* wrenNewVM(WrenConfiguration* configuration);
 // call to [wrenNewVM].
 void wrenFreeVM(WrenVM* vm);
 
+// A generic allocation function that handles all explicit memory management.
+// It's used like so:
+//
+// - To allocate new memory, [memory] is NULL and [oldSize] is zero. It should
+//   return the allocated memory or NULL on failure.
+//
+// - To attempt to grow an existing allocation, [memory] is the memory,
+//   [oldSize] is its previous size, and [newSize] is the desired size.
+//   It should return [memory] if it was able to grow it in place, or a new
+//   pointer if it had to move it.
+//
+// - To shrink memory, [memory], [oldSize], and [newSize] are the same as above
+//   but it will always return [memory].
+//
+// - To free memory, [memory] will be the memory to free and [newSize] and
+//   [oldSize] will be zero. It should return NULL.
+void* wrenReallocate(WrenVM* vm, void* memory, size_t oldSize, size_t newSize);
+
 // Immediately run the garbage collector to free unused memory.
 void wrenCollectGarbage(WrenVM* vm);
 
